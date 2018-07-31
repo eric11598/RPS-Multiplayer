2200981db51d81d5ca6d1fe3dfbffad506981c9c
eric11598 <eric.arellano.0803@gmail.com>
2018-07-31 00:39:57 -0400

    firebase loaded

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB6yF_7bMgO0cr0hYf1wLrvQLE3I9Z6FJ8",
+    authDomain: "rps-multiplayer-8a6ac.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-8a6ac.firebaseio.com",
+    projectId: "rps-multiplayer-8a6ac",
+    storageBucket: "rps-multiplayer-8a6ac.appspot.com",
+    messagingSenderId: "50392520770"
+  };
+  
+  firebase.initializeApp(config);
