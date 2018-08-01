67b31ffead4945884818d6077160a286656514c6
eric11598 <eric.arellano.0803@gmail.com>
2018-08-01 02:23:31 -0400

    added functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,80 +22,184 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var ref = database.ref();
 
+$("#playerOneButtonContainer").hide();
+$("#playerTwpButtonContainer").hide();
+
 var usersRef = ref.child("users");
+var turnRef = ref.child("users");
+
+var playerOne = false;
+var playerTwo = false;
+var gameMode = 0; //1 when game is playing
+var turn = 1; //swaps between 1 and 2 for whos turn it is 
+
+
+
 
 ref.on("value", function(snapshot) {
 
+var usersRef = ref.child("users");
+console.log(usersRef);
 
-    console.log("YOU ARE HERE NOW");
-    usersRef.set({
-      one: {
-        name:"",
-       
-      },
-      two: {
-       name:"",
-      }
-    });
+//Checks to see if players are in game by pulling the name object
 
-    ref.child("turn").set({
-      value: 1});
+if(snapshot.val().users.one.name==="")
+  {
+    playerOne = false;
+    $("#playerOneName").text("Waiting for player 1");
+  }
 
+if(snapshot.val().users.two.name==="")
+  {
+
+    playerTwo = false
+    $("#playerTwoName").text("Waiting for player 2");
+    
+  }
+
+if(snapshot.val().users.one.name)
+  {
+    playerOne = true;
+    $("#playerOneName").text(snapshot.val().users.one.name);
+    
+  }
+
+if(snapshot.val().users.two.name)
+  {
+    playerTwo = true;
+    $("#playerTwoName").text(snapshot.val().users.two.name);
+   
+  }
+
+//If both players exist, game mode starts
+if(snapshot.val().users.one.name && snapshot.val().users.two.name)
+{
+  console.log("HURRRRRRR");
+
+  if(turn===1)
+  {
+    console.log("1");
+    $("#playerOneButtonContainer").show();
+    $("#playerTwoButtonContainer").hide();
+  }
+  if(turn===2)
+  {
+    console.log("2");
+    $("#playerOneButtonContainer").hide();
+    $("#playerTwoButtonContainer").show();
+
+  }
+
+  /*
+  var buttonArray = ['playerOneRock', 'playerOnePaper', 'playerOneScissor', 'playerTwoRock', 'playerTwoPaper', 'playerTwoScissor'];
   
-  console.log(snapshot.val());
+  for(var i = 0; i<buttonArray.length; i++)
+  {
+    var container;
+    var player;
+    gameMode = 1;
+
+    if(i>2)
+    {
+      container = '#playerTwoButtonContainer';
+      player = 'playerTwo';
+    }
+    else
+    {
+      container = '#playerOneButtonContainer';
+      player = 'playerOne';
+    }
+
+      $('<button/>', {
+        id: buttonArray[i],
+        class: 'btn btn-secondary btn-sm',
+        value: buttonArray[i],
+        player: player,
+        text: buttonArray[i],  
+        }).appendTo(container);
+    
+}*/
+
+
+}
+  
+
   
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
 
 
+function test() {
+  usersRef.set({
+    one: {
+      name:"",
+     
+    },
+    two: {
+     name:"",
+    }
+  });
 
+}
 
+//test();
+$(document).on("click", ".btn-secondary", runGame);
 
+function runGame(){
+  console.log("LOOL");
+  var moveTemp = this.value
+  var player = (this.value).substring(0, 9);
+
+  console.log(player+"  "+moveTemp);
+  
+  if(player === "playerOne")
+  {
+    firebase.database().ref('users/one/').update({
+      move: moveTemp,
+    });
+    turn = 2;
+  }
+  
+
+}
 
 // 2. Button for adding Employees
-$("#add-employee-btn").on("click", function(event) {
+$("#add-user").on("click", function(event) {
   event.preventDefault();
+  
 
   // Grabs user input
-  var empName = $("#employee-name-input").val().trim();
-  var empRole = $("#role-input").val().trim();
-  var empStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
-  var empRate = $("#rate-input").val().trim();
+  var name = $("#name-input").val().trim();
+  
+  if(!playerOne)
+    usersRef.update({
+      one: {
+        name: name,
+        wins: 0,
+        losses: 0,
+        move: '',
+      }});
 
-  // Creates local "temporary" object for holding employee data
-  var newEmp = {
-    name: empName,
-    role: empRole,
-    start: empStart,
-    rate: empRate
-  };
-
-  // Uploads employee data to the database
-  database.ref().set()
-  database.ref().push(newEmp);
-
-  // Logs everything to console
-  console.log(newEmp.name);
-  console.log(newEmp.role);
-  console.log(newEmp.start);
-  console.log(newEmp.rate);
-
-  alert("Employee successfully added");
-
-  // Clears all of the text-boxes
-  $("#employee-name-input").val("");
-  $("#role-input").val("");
-  $("#start-input").val("");
-  $("#rate-input").val("");
+  else if(!playerTwo)
+    usersRef.update({
+      two: {
+        name: name,
+        wins: 0,
+        losses: 0,
+        move:'',
+      }});
+  
 });
 
+
+
 // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-/*database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function(childSnapshot) {
   console.log(childSnapshot.val());
+  
 
-
-});*/
+});
 
 // Example Time Math
 // -----------------------------------------------------------------------------
